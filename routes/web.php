da403b7bd667447382ab787738eb61f20afe1faf
Madhu Mohan <maddy.10m@gmail.com>
2024-10-19 22:08:04 -0500

    Testing reroute to external

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/video', function () {
+    return Redirect::to('www.youtube.com');
+});
