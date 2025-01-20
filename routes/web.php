49ccbe3035df6b65801df5273092b58d516e11e6
Madhu Mohan <maddy.10m@gmail.com>
2025-01-20 14:26:53 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,19 @@ Route::get('/', function () {
 Route::get('/video', function () {
     return redirect()->away("https://www.google.com");
 });
+
+Route::get('/tenali1', function () {
+    return redirect()->away("https://www.youtube.com/playlist?list=PL0uP6KE3osm5HThdOBd20NiGnTo4dyhes");
+});
+
+Route::get('/bluefox', function () {
+    return redirect()->away("https://www.youtube.com/watch?v=Ef-zqBLYulM");
+});
+
+Route::get('/cagedtiger', function () {
+    return redirect()->away("https://www.youtube.com/watch?v=SfbvXq9bdgE");
+});
+
+Route::get('/hungrylion', function () {
+    return redirect()->away("https://www.youtube.com/watch?v=HiXHQ6J8_MA");
+});
